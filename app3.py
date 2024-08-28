e0ba8c66b7248c1ddd35df0d53dd3efee3474a13
kirtisikka <kirtisikka972@gmail.com>
2024-08-28 23:33:53 +0530

    added

--- app3.py
+++ app3.py
@@ -36,10 +36,9 @@ def process_image(image_path):
     # Load Layout model
     model = lp.PaddleDetectionLayoutModel(
         config_path="lp://PubLayNet/ppyolov2_r50vd_dcn_365e_publaynet/config",
-        threshold=0.5,
+  
         label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"},
-        enforce_cpu=False,
-        enable_mkldnn=True
+      
     )
 
     # Read image
