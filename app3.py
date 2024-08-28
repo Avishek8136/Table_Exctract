8a73d87b49a12e23d9dc796b0ec9014b47fd77f9
kirtisikka <kirtisikka972@gmail.com>
2024-08-29 00:34:28 +0530

    added

--- app3.py
+++ app3.py
@@ -11,22 +11,7 @@ from PIL import Image
 import layoutparser as lp
 from paddleocr import PaddleOCR
 
-# Ensure necessary packages are installed
-# def install_packages():
-#     try:
-#         import fitz  # PyMuPDF
-#     except ImportError:
-#         subprocess.check_call([sys.executable, "-m", "pip", "install", "pymupdf"])
 
-#     try:
-#         from paddleocr import PaddleOCR
-#     except ImportError:
-#         subprocess.check_call([sys.executable, "-m", "pip", "install", "paddlepaddle", "paddleocr"])
-
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", "layoutparser"])
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", "protobuf==3.20.0"])
-
-# install_packages()
 
 # Define the main processing function
 def process_image(image_path):
